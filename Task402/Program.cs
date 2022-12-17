b8225d90c4c2ed7618225fbc42706e110c77bea0
Mojo-Lan <Sve9rod@gmail.com>
2022-12-17 12:58:44 +0700

    task402_edited

--- Task402/Program.cs
+++ Task402/Program.cs
@@ -4,7 +4,22 @@
 // 9012 -> 12
 // Строки использовать нельззя
 
+class SumOfNumbers
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");                        //Вводим произвольное число
+        int i = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;                                                //Присваиваем параметру сумма значение 0
 
-
+        while (i > 0)                                               //цикл вычисление цифры и сложение цифр числа
+        {
+            int number = i % 10;
+            i = i / 10;
+            sum += number;
+        }
+        Console.WriteLine("Сумма всех цифр в числе равна " + sum);      //Вывод суммы цифр числа
+    }
+}
 
 

b11f1c619b5c9d9f90e0dbfaacbca65798b005a4
Mojo-Lan <Sve9rod@gmail.com>
2022-12-18 16:28:39 +0700

    correctMyLabrary

--- Task403/Program.cs
+++ Task403/Program.cs
@@ -1,11 +1,24 @@
-﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов (из диапазона [0-14] ) 
+﻿using static MyLibrary;
+
+// Задача 29: Напишите программу, которая задаёт массив из N элементов (из диапазона [0-14] ) 
 // и выводит на экран десятичное представление числа, записанного в СС по основанию 15
 // N: 3 [9, 4, 12] => 2097
-
-int n = 10;
-for (int i = 0; i < n; i++)
+public static class ArrayN
 {
-    Console.WriteLine($"{i}^3 = {i * i * i}");
+    public static void Main()
+    {
+        // int n = 10;
+        // for (int i = 0; i < n; i++)
+        // {
+        //     Console.WriteLine($"{i}^3 = {i * i * i}");
+        // }
+        // //Console.WriteLine(MyLibrary.Pow(2, 10));
+        
+        int size = Input("Сколько чисел в массиве: ");
+        int[] ar = CreateArray(size);
+        Fill(ar);
+        Console.WriteLine(Print(ar));
+        Console.WriteLine(MyLibrary.ConversionArray(ar));
+
+    }
 }
-//Console.WriteLine(MyLibrary.Pow(2, 10));
-Console.WriteLine(MyLibrary.ToDec(new int[] { 9, 4, 12 }));
